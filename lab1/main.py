f3600620fc68d974a83372ffca2ff9ca178656c8
alex <alexyakimchyk@gmail.com>
2019-11-07 01:43:44 +0200

    added plot

--- lab1/main.py
+++ lab1/main.py
@@ -1,16 +1,19 @@
 from lab1 import choices
 from lab1.agents import RandomAgent, ReflectiveAgent, ModelBasedAgent
 from lab1.environment import Environment
-from lab1.utils import Position
+from lab1.utils import Position, show
 
 start_position = Position(1, 1)
-iterations = 10000
+iterations = 1000
 maps = [choices.MAP1, choices.MAP2, choices.MAP3]
 agents = (RandomAgent, ReflectiveAgent, ModelBasedAgent)
 
 if __name__ == "__main__":
 
     for map in maps:
+        agent_names = ('random', 'reflective', 'model based')
+        garbage_list = []
+        energy_list = []
         print(map)
         for agent_class in agents:
             env = Environment(map)
@@ -19,3 +22,6 @@ if __name__ == "__main__":
             garbage, energy = env.run(agent=agent,
                                       iterations=iterations)
             print(garbage, energy)
+            garbage_list.append(garbage)
+            energy_list.append(energy)
+        show(agent_names, garbage_list, energy_list)
--- lab1/utils.py
+++ lab1/utils.py
@@ -1,11 +1,14 @@
 from collections import namedtuple
+import matplotlib.pyplot as plt
+import numpy as np
 
 Size = namedtuple('Size', ('len', 'width'))
 Position = namedtuple('Position', ('x', 'y'))
 
 
 class Node:
-    def __init__(self, count=1, total_weight=float('inf'), pos=None, from_node=None, visited=False):
+    def __init__(self, count=1, total_weight=float('inf'), pos=None,
+                 from_node=None, visited=False):
         self.count = count
         self.total_weight = total_weight
         self.pos = pos
@@ -13,7 +16,8 @@ class Node:
         self.visited = visited
 
     def show(self):
-        print(f'count: {self.count} total: {self.total_weight} from: {self.from_node} visited: {self.visited}')
+        print(
+            f'count: {self.count} total: {self.total_weight} from: {self.from_node} visited: {self.visited}')
 
 
 def energy_required(energy):
@@ -26,3 +30,22 @@ def energy_required(energy):
         return inner
 
     return _energy_required
+
+
+def show(labels, garbage, energy):
+    x = np.arange(len(labels))
+    width = 0.45
+
+    fig, ax = plt.subplots()
+    ax.bar(x - width / 2, garbage, width, label='garbage')
+    ax.bar(x + width / 2, energy, width, label='enegry')
+
+    ax.set_ylabel('Scores')
+    ax.set_title('Scores by agents')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+    fig.tight_layout()
+
+    plt.show()
